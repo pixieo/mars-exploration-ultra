965de7456c82ae48fa5be3d58e72b3ec0fb4f3f6
pixieo <saratayari07@gmail.com>
2023-10-26 12:41:01 +0300

    Refactor Exploration component to inherit Item

--- mars-exploration-fe/src/pages/ExplorationCreator.jsx
+++ mars-exploration-fe/src/pages/ExplorationCreator.jsx
@@ -1,45 +1,15 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import ExplorationForm from "../components/ExplorationForm";
-
-const createExploration = (input) => {
-  return fetch("http://localhost:8080/exploration", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(input),
-  })
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error("Failed.");
-    }
-    return res.json();
-  })
-  .catch((err) => {
-    console.error("Error:", err);
-  });
-};
+import ItemCreator from "../services/ItemCreator";
 
 const ExplorationCreator = () => {
-  const navigate = useNavigate();
-
-  const handleExploration = (input) => {
-    createExploration(input)
-      .then((result) => {
-        console.log("Exploration created successfully. Id:", result);
-        navigate("/result/exploration");
-      })
-      .catch((err) => {
-        console.error("Failure", err);
-      });
-  };
-
-  return (
-    <ExplorationForm 
-        onSave={handleExploration}
-    />
-  );
+    return (
+        <ItemCreator
+            formComponent={ExplorationForm}
+            url="exploration"
+            successMessage="Exploration created successfully"
+        />
+    );
 };
 
 export default ExplorationCreator;
